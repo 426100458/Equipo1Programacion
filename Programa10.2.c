7c410bb7c79bf2caac6ac4bf3b948a30b796e00c
Evelyn Hernandez <eve@Evelyns-MacBook-Air.local>
2025-11-17 19:49:36 -0600

    Correción de programa 10

--- Programa10.2.c
+++ Programa10.2.c
@@ -1,65 +1,101 @@
+// BIBLIOTECAS
 #include <stdio.h>
 #include <stdlib.h>
 
+// PROTOTIPOS
 void Caratula();
 void ObtenerDivisores(int);
 
-void Caratula()
+// FUNCIÃ“N PRINCIPAL
+
+int main()
 {
-    puts("Equipo 1\n"
-        "Integrantes:\n"
-        "1. Contreras Aviles Jose Raul.\n"
-         "2. Hernandez Tovar Evelyn Ariadna.\n"
-         "3. Rosas Fragoso Leonardo.\n"
-         "4. Rosas Vazquez Eduardo.\n"
-         "5. Zamitiz Carmona Cristian Erasto.");
-    printf("Presiona Enter");
-    getchar();
-    system("clear");
+
+	int num, repetir;
+	// FunciÃ³n para mostrar la carÃ¡tula
+	Caratula();
+
+	// Bucle para repetir el programa en caso de que el usuario lo desee
+	do
+	{
+
+		printf("Ingrese un nÃºmero: ");
+		scanf("%d", &num);
+
+		if (num != 0)
+		{
+			// FunciÃ³n para obtener los divisores de un numero entero
+			ObtenerDivisores(num);
+		}
+		else
+		{
+			printf("El nÃºmero 0 no tiene divisores.\n");
+		}
+		printf("Â¿Desea realizar otra operaciÃ³n? 1. Si, (Cualquier nÃºmero).- No\n");
+		scanf("%d", &repetir);
+
+		// Limpieza de pantalla o salida
+		if (repetir == 1)
+		{
+			system("clear");
+		}
+		else
+		{
+			printf("\nSaliendo...\n");
+		}
+	} while (repetir == 1);
+
+	return 0;
 }
 
-void ObtenerDivisores(int n) {
+// FUNCIONES
+void Caratula()
+{
+	// Titulo
+	puts("\nPrograma 10 - Divisores.\n");
+
+	// Descripccion
+	puts("Este programa obtiene todos los divisores de un nÃºmero entero N.\n");
+
+	// Integrantes
+	puts("1. Contreras Aviles JosÃ© RaÃºl.\n"
+		 "2. HernÃ¡ndez Tovar Evelyn Ariadna.\n"
+		 "3. Rosas Fragoso Leonardo.\n"
+		 "4. Rosas VÃ¡zquez Eduardo.\n"
+		 "5. Zamitiz Carmona Cristian Erasto.\n");
+
+	// Pausa
+	printf("Presiona cualquier tecla para continuar...");
+	getchar();
+
+	// Limpiar consola
+	system("clear");
+}
+void ObtenerDivisores(int n)
+{
 	printf("Sus divisores de %d son: ", n);
 
-	if (n > 0) {
-		for(int i = 1; i <= n; i++) {
-			if(n % i == 0) {
+	// Caso de un numero positivo
+	if (n > 0)
+	{
+		for (int i = 1; i <= n; i++)
+		{
+			if (n % i == 0)
+			{
 				printf("%d ", i);
 			}
 		}
-	} else {
-		for(int i = n; i <= -n; i++) {
-			if(i != 0 && n % i == 0) {
+		// Caso de un numero negativo
+	}
+	else
+	{
+		for (int i = n; i <= -n; i++)
+		{
+			if (i != 0 && n % i == 0)
+			{
 				printf("%d ", i);
 			}
 		}
 	}
 	printf("\n");
-}
-
-int main()
-{
-	int num, repetir;
-	Caratula();
-	do {
-	    puts("Programa 10 (Divisores).");
-	    puts("Obtener todos los divisores de un número entero N.\n"
-	    "El programa debe contener una función que se llame 'ObtenerDivisores' que reciba como parámetro un número entero y que imprima los divisores encontrados de ese número.");
-		printf("Ingrese un número: ");
-		scanf("%d", &num);
-		if(num != 0) {
-			ObtenerDivisores(num);
-		} else {
-			printf("El número 0 no tiene divisores.\n");
-		}
-		printf("¿Desea realizar otra operación? 1. Sí, (Cualquier otro número).- No\n");
-		scanf("%d", &repetir);
-		if(repetir==1) {
-			system("clear");
-		}
-		else {
-			printf("\nSaliendo...\n");
-		}
-	} while(repetir==1);
-	return 0;
 }
