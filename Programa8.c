df66d9dd85856659fedfac1f691da7bcc8ad35d7
426100458 <426100458@pcpuma.acatlan.unam.mx>
2025-11-22 11:56:12 -0600

    Programa8.2.c
    comentarios

--- Programa8.c
+++ Programa8.c
@@ -1,34 +1,37 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+// Función carátula
 void Caratula(){
     puts("Programa 8 (Factorial)\nCalcular el factorial de un número entero positivo (no usar recursividad)");
     puts("Equipo 1\n"
-         "Integrantes:\n"
-         "1. Contreras Aviles José Raúl.\n"
-         "2. Hernández Tovar Evelyn Ariadna.\n"
-         "3. Rosas Fragoso Leonardo.\n"
-         "4. Rosas Vázquez Eduardo.\n"
-         "5. Zamitiz Carmona Cristian Erasto.\n");
-    puts("Presiona Enter");
-    getchar();
-    system("clear");
+        "Integrantes:\n"
+        "1. Contreras Aviles José Raúl.\n"
+        "2. Hernández Tovar Evelyn Ariadna.\n"
+        "3. Rosas Fregoso Leonardo.\n"
+        "4. Rosas Vázquez Eduardo.\n"
+        "5. Zamitiz Carmona Cristian Erasto.\n");
+        puts("Presiona Enter");
+        getchar();
+        system("clear");
 }
 
+// Función para limpiar el buffer
 void Limpiar() {
     int c;
     while ((c = getchar()) != '\n' && c != EOF);
 }
 
+// Función del Factorial
 void Factorial(){
     int factorial, eleccion;
-    unsigned long long resultado; 
+    unsigned long long resultado;
 
     do{
-        puts("Ingresa el número al que deseas encontrar su factorial (no mayor a 20)): ");
-        
+        puts("Ingresa el número entero al que deseas encontrar su factorial (no mayor a 20)): ");
+
         if (scanf("%d", &factorial) != 1) {
-            puts("Error: Entrada no válida (debe ser un número entero).");
+            puts("Entrada no válida (debe ser un número entero).");
             Limpiar();
             factorial = -1;
         }
@@ -37,35 +40,31 @@ void Factorial(){
         if (factorial < 0){
             puts("El factorial no está definido para números negativos.");
         } else if (factorial > 20){
-            puts("El número es demasiado grande, el resultado será incorrecto con unsigned long long (20! es el límite).");
+            puts("El número es demasiado grande, el resultado **será** incorrecto(20! es el límite).");
         } else {
             resultado = 1;
             
-            if (factorial > 0) {
-                for (int i = 1; i <= factorial; i++){
-                    resultado *= i;
+        if (factorial > 0) {
+            for (int i = 1; i <= factorial; i++){
+                resultado *= i;
                 }
-            }
-
-            printf("El factorial de %d es: %llu\n", factorial, resultado);
         }
-        
-        puts("Deseas repetir el programa (1=si)?");
-        scanf("%d", &eleccion);
-        if (eleccion != 1) {
-             eleccion = 0;
+
+        printf("El factorial de %d es: %llu\n", factorial, resultado);
+        }
+
+        puts("¿Deseas repetir el programa (1=si)?");
+        if (scanf("%d", &eleccion) != 1) {
+        eleccion = 0;
         }
         Limpiar();
 
         system("clear");
-        
-        if(eleccion != 1){
-            puts("Fin del programa");
-        }
-        
-    } while (eleccion == 1);
-}
 
+    } while (eleccion == 1);
+
+    puts("Fin del programa");
+}
 int main(){
     Caratula();
     Factorial();
